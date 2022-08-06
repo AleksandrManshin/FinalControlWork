77e28617f1c007509812d1aec6ca5497b1c4efc7
Aleksandr <maivan007@yandex.ru>
2022-08-06 11:50:42 +0400

    Add main method

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿void MainMethod()
+{
+    Console.Write("Введите количество слов которое хотите ввести: ");
+    int size = Convert.ToInt32(Console.ReadLine());
+    string[] array = new string[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine("Введите слово: ");
+        array[i] = Console.ReadLine();
+    }
+}
+
+MainMethod();
